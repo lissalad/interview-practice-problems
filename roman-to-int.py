7dc3045f242c60295b3ce1134e4e4b3541afe67d
lissalad <elissalaymon@gmail.com>
2022-03-23 11:10:11 -0700

    two sum

--- roman-to-int.py
+++ roman-to-int.py
@@ -1,3 +1,17 @@
+#     https://leetcode.com/problems/roman-to-integer/
+
+# PROBLEM: 
+# given a string of roman numerals, return the integer value
+
+# PSUEDOCODE:
+# create dictionary of single character values
+# create dictionary of two character subtractive values
+# loop through string for subtractive values
+# if found, add its value to the running sum, and remove two characters from string
+# loop through remaining characters for single character values
+# if found, add its value to the running sum, and remove from string
+# return sum value
+
 values = {
   "I": 1,
   "V": 5,
---
+++ two-sum.py
@@ -0,0 +1,32 @@
+#    https://leetcode.com/problems/two-sum/
+
+# PROBLEM:
+# given a list of numbers and a target number, return the list indices of
+# two number in the list with a sum of that target number
+
+# ASSUME:
+# each input has exactly one solution
+# each element can be used once
+
+# PSUEDOCODE:
+# create loop to repeat until target sum is reached
+# loop through nums. set goal for what would create the sum
+# if sum exists in nums list, and is not the same as the current num selected, return each index, break loop
+# otherwise start again using next num in list
+
+
+def two_sum(nums, target):
+  sum = 0
+  goal = 0
+  targetReached = False
+
+  while not targetReached:
+    for num in nums:
+      goal = target - num
+      if goal in nums:
+        if not nums.index(num) == nums.index(goal):
+          targetReached = True
+          return [nums.index(num), nums.index(goal)]
+
+
+print(two_sum([2, 7, 11, 15], 9))
