224992f4e2aaa550af6edf3a1ed9808b1fecb838
lissalad <elissalaymon@gmail.com>
2022-04-10 19:32:30 -0700

    length of last word

---
+++ human_pyramid.py
@@ -0,0 +1,20 @@
+# PROBLEM:
+# Taller, heavier people have to be below shorter, lighter, people in the tower.
+# Given people’s height and weight, construct a tower of people. 
+# How tall is the maximum size tower?
+
+# QUESTIONS:
+# What if we get an odd number of people?
+
+
+# SIMPLIFY:
+# smaller people on the bottom
+
+
+    #
+   # #
+  # # #
+ # # # #
+# # # # #
+
+
---
+++ last_word_length.py
@@ -0,0 +1,29 @@
+#       https://leetcode.com/problems/length-of-last-word/
+
+# PROBLEM:
+# given a string with chunks separated by spaces,
+# return the length of the last word not including spaces
+
+# PSEUDOCODE:
+# loop through string backwords until a non-space character
+# loop from that index keep count of the non-space characters
+# when space is reached, return count
+ 
+
+def last_word_length(string):
+  start = len(string) - 1
+
+  while string[start] == " ":
+    start -= 1
+
+  end = start
+
+  while not string[end] == " ":
+    end -= 1
+  
+  return start - end
+
+
+print(last_word_length("luffy is still joyboy"))
+  
+
