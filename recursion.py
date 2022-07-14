339cd1f74e7483cece4c5e3ad158ba978d91f723
lissalad <elissalaymon@gmail.com>
2022-07-14 12:43:52 -0700

    incomplete sort_colors

---
+++ recursion.py
@@ -0,0 +1,11 @@
+# PROBLEM
+# recursive factorial
+
+# GIVEN:
+# an int, return its factorial
+
+def factorial(num):
+
+
+
+print(factorial(4))
--- sort_colors.py
+++ sort_colors.py
@@ -7,12 +7,18 @@
 
 # PSEUDOCODE:
 # loop through list
+# if not first in list, check that current is not lesser than previous
+# if so, switch them
+
+
+# ATTEMPT
+# incomplete :/
 
 def sort_colors(nums):
   for index in range(len(nums)):
     if index > 0:
-      while nums[index-1] < nums[index]:
+      while nums[index-1] > nums[index]:
         nums[index], nums[index-1] = nums[index-1], nums[index]
   return nums
 
-print(sort_colors([0,1,2]))
+print(sort_colors([0,1,2,1,3]))
